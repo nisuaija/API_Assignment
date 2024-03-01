4b43e37fa6067f7b0608c33de81708c25942d8b3
nisuaia <eppu.nissinen@gmail.com>
2024-03-01 13:46:15 +0200

    Docker setup

--- API/API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b1a410a64a17b22f3cea4963c57a7bf4b94b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19700f10f2088b828ed0850f3e107e858387fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
